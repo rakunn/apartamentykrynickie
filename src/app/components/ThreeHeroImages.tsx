aad656a0c17fec2b24ded74ee493e43b0857205f
rafalbagrowski <rbagrowsk@gmail.com>
2024-06-07 13:17:11 -0600

    added object fit cover to ThreeHeroImages.tsx

--- src/app/components/ThreeHeroImages.tsx
+++ src/app/components/ThreeHeroImages.tsx
@@ -5,13 +5,13 @@ export const ThreeHeroImages = () => {
     <div className="relative">
       <div className="h-[33vh] flex">
         <div className="relative flex-[1_0_0]">
-          <Image src="/magnolia/2.jpg" alt="" fill />
+          <Image src="/magnolia/2.jpg" alt="" fill className="object-cover" />
         </div>
         <div className="relative flex-[1_0_0]">
-          <Image src="/magnolia/1.jpg" alt="" fill />
+          <Image src="/magnolia/1.jpg" alt="" fill className="object-cover" />
         </div>
         <div className="relative flex-[1_0_0]">
-          <Image src="/storczyk/6.jpg" alt="" fill />
+          <Image src="/storczyk/6.jpg" alt="" fill className="object-cover" />
         </div>
       </div>
       <h1 className="absolute text-nowrap p-4 text-3xl bg-white text-primary w-fit bottom-4 left-1/2 -translate-x-1/2 rounded-lg shadow-xl">
